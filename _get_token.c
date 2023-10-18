d051a3dbf7f6b9e9a25706e0e169a71fb41354c2
ahmedkhaledelsayed <ahmed.khaled499453@gmail.com>
2023-10-19 00:41:13 +0300

    ss

--- _get_token.c
+++ _get_token.c
@@ -7,10 +7,10 @@
 
 char **_token(char *lineptr)
 {
-	char **command = NULL;
+	char **user_command = NULL;
 	char *token = NULL;
 	size_t i = 0;
-	int s = 0;
+	int size = 0;
 
 	if (lineptr == NULL)
 		return (NULL);
@@ -18,20 +18,20 @@ char **_token(char *lineptr)
 	for (i = 0; lineptr[i]; i++)
 	{
 		if (lineptr[i] == ' ')
-			s++;
+			size++;
 	}
-	if ((s + 1) == _strlen(lineptr))
+	if ((size + 1) == _strlen(lineptr))
 		return (NULL);
-	command = malloc(sof(char *) * (s + 2));
-	if (command == NULL)
+	user_command = malloc(sizeof(char *) * (size + 2));
+	if (user_command == NULL)
 		return (NULL);
 
 	token = _strtok(lineptr, " \n\t\r");
 	for (i = 0; token != NULL; i++)
 	{
-		command[i] = token;
+		user_command[i] = token;
 		token = _strtok(NULL, " \n\t\r");
 	}
-	command[i] = NULL;
-	return (command);
+	user_command[i] = NULL;
+	return (user_command);
 }
