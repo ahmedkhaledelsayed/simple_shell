1f2c3ad21f13ab453aa3ec11e127b52b85756607
ahmedkhaledelsayed <ahmed.khaled499453@gmail.com>
2023-10-19 00:09:28 +0300

    exit testing

--- _bulitin.c
+++ _bulitin.c
@@ -8,7 +8,7 @@
  */
 void _exit_fun(char **arg, char *lineptr, int _exit)
 {
-	int exit_status = 0;
+	int status = 0;
 
 	if (!arg[1])
 	{
@@ -16,20 +16,20 @@ void _exit_fun(char **arg, char *lineptr, int _exit)
 		free(arg);
 		exit(_exit);
 	}
-	exit_status = atoi(arg[1]);
+	status = atoi(arg[1]);
 
 	free(lineptr);
 	free(arg);
-	exit(exit_status);
+	exit(status);
 }
 
 /**
- *_env_func - function to get all env
+ *_getenv - function to get all env
  *@env: enviroment
  *Return: 0
  */
 
-void _env_func(char **env)
+void _getenv(char **env)
 {
 	size_t run = 0;
 
